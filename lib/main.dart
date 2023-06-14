9de4ff2e37090ad3e7687fa64377a66395022e63
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-06-14 12:01:00 +0200

    Implement Flutter_Modular package to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_introduction_app/app/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  return runApp(ModularApp(module: AppModule(), child: const MyApp()));
+}
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute('/', child: (context, args) => const HomePage()),
+      ];
 }
 
 class MyApp extends StatelessWidget {
@@ -10,14 +21,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+      theme: ThemeData(primarySwatch: Colors.purple, useMaterial3: true),
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
     );
   }
 }
