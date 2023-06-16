d50ac9ac465570bdc66f7fa834ff131f12567e33
Jakub Olszewski <kuba.olszewski.0704@icloud.com>
2023-06-16 10:21:34 +0200

    Initialize easy_localization and generate transcriptions

---
+++ lib/l10n/locale_keys.g.dart
@@ -0,0 +1,17 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const firstHeader = 'firstHeader';
+  static const secondHeader = 'secondHeader';
+  static const firstSecondHeader = 'firstSecondHeader';
+  static const thirdHeader = 'thirdHeader';
+  static const firstPageText = 'firstPageText';
+  static const firstSecondPageText = 'firstSecondPageText';
+  static const secondPageText = 'secondPageText';
+  static const thirdPageText = 'thirdPageText';
+  static const firstPageNav = 'firstPageNav';
+  static const firstSecondPageNav = 'firstSecondPageNav';
+  static const secondPaveNav = 'secondPaveNav';
+  static const thirdPageNav = 'thirdPageNav';
+
+}
